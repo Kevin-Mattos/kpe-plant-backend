bdbf2ae73e3091175b07a6820f747ccdc0bcff69
Kevin-Mattos <kevinmmattos@gmail.com>
2022-03-28 21:33:22 -0300

    rename variables

--- main.go
+++ main.go
@@ -20,69 +20,61 @@ func main() {
 	// createDetails(detailsRepo)
 	a := getDetails(detailsRepo)
 
-	for _, user := range a {
-		fmt.Printf("%d %s\n", user.ID, user.Name)
+	for _, detail := range a {
+		fmt.Printf("%d %s\n", detail.ID, detail.Name)
 	}
 
-	users := getPlants(repo)
-	for _, user := range users {
-		fmt.Printf("%d %s %d\n", user.ID, user.Nome, user.Idade)
+	plants := getPlants(repo)
+	for _, plant := range plants {
+		fmt.Printf("%d %s %d\n", plant.ID, plant.Nome, plant.Idade)
 	}
 
 }
 
 func getPlant(repo plantDS.PlantRepository, id int) *entities.Plant {
-	user, _ := repo.GetPlant(id)
-	return user
+	plant, _ := repo.GetPlant(id)
+	return plant
 }
 
 func getPlants(repo plantDS.PlantRepository) []*entities.Plant {
-	users, _ := repo.GetPlants()
-	return users
+	plants, _ := repo.GetPlants()
+	return plants
 }
 
 func createPlant(repo plantDS.PlantRepository) *entities.Plant {
-	user := entities.Plant{
+	plant := entities.Plant{
 		Nome:  "nominho",
 		Idade: 123,
 	}
 
-	repo.CreatePlant(&user)
-	return &user
+	repo.CreatePlant(&plant)
+	return &plant
 }
 
 func deletePlant(repo plantDS.PlantRepository) *entities.Plant {
-	user := entities.Plant{
+	plant := entities.Plant{
 		ID: 3,
 	}
 
-	repo.DeletePlant(user.ID)
-	return &user
+	repo.DeletePlant(plant.ID)
+	return &plant
 }
 
 func getDetails(repo plantDetailsDS.PlantDetailsRepository) []*entities.Details {
-	users, _ := repo.GetDetails()
-	return users
+	details, _ := repo.GetDetails()
+	return details
 }
 
 func createDetails(repo plantDetailsDS.PlantDetailsRepository) *entities.Details {
-	user := entities.Details{
+	detail := entities.Details{
 		Name: "nominho",
 	}
 
-	_, err := repo.CreateDetails(&user)
+	_, err := repo.CreateDetails(&detail)
 
 	if err != nil {
 		panic(err)
 	}
 
-	return &user
+	return &detail
 }
-
-// func Teste[T any](value T) any {
-// 	var t T
-// 	fmt.Printf("%T", t)
-
-// 	var plant entities.Plant
-// 	return {&plant.ID, &plant.Nome, &plant.Idade}
-// }
