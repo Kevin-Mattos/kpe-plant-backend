63045337deea32ecd028487b8fc4fb67b6452af9
Kevin-Mattos <kevinmmattos@gmail.com>
2022-03-30 22:21:59 -0300

    use sqlx for db con

--- datasource/myDataBase.go
+++ datasource/myDataBase.go
@@ -1,34 +1,37 @@
 package datasource
 
 import (
-	"database/sql"
 	"fmt"
 	"plant_api/secrets"
+
+	"github.com/jmoiron/sqlx"
 )
 
-var db *sql.DB
+var db *sqlx.DB
 
 var schema = `
 	CREATE TABLE IF NOT EXISTS DETAILS(id serial, name text)
 `
 
-func CreateDatabase() *sql.DB {
+func CreateDatabase() *sqlx.DB {
 	Connect()
 	return db
 }
 
 func Connect() {
+
 	var DataSourceName = fmt.Sprintf("postgresql://%s:%s@%s:%d/%s", secrets.User, secrets.Password, secrets.Host, secrets.Port, secrets.Dbname)
 	fmt.Println(DataSourceName)
 
 	var err error
-	db, err = sql.Open("postgres", DataSourceName)
-	db.Exec(schema)
+	db, err = sqlx.Connect("postgres", DataSourceName)
+
 	if err != nil {
 		panic(err.Error())
 	} else {
 		fmt.Println("Connected!")
 	}
+	db.Exec(schema)
 }
 
 func Close() {
