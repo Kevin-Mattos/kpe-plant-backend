ae83f03069005845825e7b4815adbb7239787734
Kevin-Mattos <kevinmmattos@gmail.com>
2022-04-03 22:52:15 -0300

    feat: remove array type

--- entities/Details.go
+++ entities/Details.go
@@ -2,11 +2,9 @@ package entities
 
 import "time"
 
-type Details *[]*Detail
-
 type Detail struct {
 	ID               int       `json:"id" db:"id_detail"`
-	PlantId          int       `json:"plantId" db:"id_plant" binding:"required"`
+	PlantId          int       `json:"plantId" db:"id_plant"`
 	Time             time.Time `json:"time" db:"time"`
 	InternalHumidity float32   `json:"internalHumidity" db:"internal_humidity" binding:"required"`
 	ExternalHumidity float32   `json:"externalHumidity" db:"external_humidity" binding:"required"`
